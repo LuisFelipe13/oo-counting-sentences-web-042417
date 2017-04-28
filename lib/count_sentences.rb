8a0440414378f509baaccf80d9eaad43d4209ab3
Luis del Cueto <luisdelcueto@live.com>
2017-04-27 20:51:17 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    @counter = 0
+    values = self.split(" ")
+    values.each {|x| @counter += 1 if x[-1].end_with?(".") || x[-1].end_with?("?") || x[-1].end_with?("!")}
+    @counter
   end
 end
